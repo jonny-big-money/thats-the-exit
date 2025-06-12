9ea1ab7cad3e3c04444c1174863245c0864294a9
jonny-big-money <chenjonathan62@gmail.com>
2025-06-12 17:08:35 -0400

    2nd commit

--- thats the exit.py
+++ thats the exit.py
@@ -2,12 +2,11 @@
 # exit()
 
 for i in range(10):
-  
-
-
-if i == 5:
-
-  # prints the exit message
-  print(exit)
-  exit( )
-print(i)
+  # If the value of i becomes
+  # 5 then the program is forced
+  # to exit
+  if i== 5:
+    # prints the exit messane
+    print(exit)
+    exit()
+  print(i)
